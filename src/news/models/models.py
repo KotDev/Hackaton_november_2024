d1d90e936607d94ff98853c77b63233c1bee0b08
Surface!!u <surface812@gmail.com>
2024-11-22 21:33:30 +0300

    дополнены таблицы

--- src/news/models/models.py
+++ src/news/models/models.py
@@ -1,10 +1,36 @@
 from sqlalchemy.dialects.oracle import NUMBER
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Text
+from sqlalchemy import String, Text, DateTime
 from database import Base
+from sqlalchemy import Table, Column, ForeignKey, Integer
+from sqlalchemy.orm import relationship
+from typing import List
+
+news_tags = Table(
+    "news_tags",
+    Base.metadata,
+    Column("news_id", ForeignKey("news.news_id", ondelete="CASCADE"), primary_key=True),
+    Column("tag_id", ForeignKey("tags.tag_id", ondelete="CASCADE"), primary_key=True),
+)
 
 class News(Base):
     news_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name_news: Mapped[str] = mapped_column(String(200), nullable=False)
     url_news: Mapped[Text] = mapped_column(Text, nullable=False)
+    start_date: Mapped[DateTime] = mapped_column(DateTime, nullable= False)
+    end_date: Mapped[DateTime] = mapped_column(DateTime, nullable=True)
+
+    news: Mapped[List["News"]] = relationship(
+        "News", secondary=news_tags, back_populates="tags"
+    )
+
+
+class Tag(Base):
+    tag_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    tags: Mapped[List["Tag"]] = relationship(
+        "Tag", secondary=news_tags, back_populates="news"
+    )
+
 
--- src/profile/models/model.py
+++ src/profile/models/model.py
@@ -1,10 +1,16 @@
 from typing import List
-
 from sqlalchemy import ForeignKey, String, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-
 from database import Base
+from sqlalchemy import Table
 
+
+profile_buisness_form = Table(
+    "profile_buisness_form",
+    Base.metadata,
+    mapped_column("profile_id", ForeignKey("profiles.profile_id", ondelete="CASCADE"), primary_key=True),
+    mapped_column("buisness_form_id", ForeignKey("Buisness_form.id", ondelete="CASCADE"), primary_key=True),
+)
 
 class Profile(Base):
     __tablename__ = "profiles"
@@ -15,8 +21,18 @@ class Profile(Base):
     about_me: Mapped[str | None] = mapped_column(String(400), nullable=True, default=None)
     age: Mapped[int | None] = mapped_column(nullable=True, default=None)
     number_phone: Mapped[str | None] = mapped_column(String(11), nullable=True, default=None)
+    buisness_forms: Mapped[List["Buisness_form"]] = relationship(secondary=profile_buisness_form, back_populates="profiles")
     photos: Mapped[List["Photo"]] = relationship(back_populates="profile")
     user: Mapped["User"] = relationship(back_populates="profile")
+
+
+
+class Buisness_form(Base):
+    __tablename__ = "Buisness_form"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    Buisness_form: Mapped[Text] = mapped_column(String(50), nullable=True, default=None)
+    Form_description: Mapped[Text] = mapped_column(String(50), nullable=True, default=None)
+    profiles: Mapped[List["Profile"]] = relationship(secondary=profile_buisness_form, back_populates="buisness_forms")
 
 class Photo(Base):
     __tablename__ = "photos"
